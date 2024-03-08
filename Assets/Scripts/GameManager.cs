f4f7891dfa2d4cd1d3590971cce8b531efb4cd9a
IvanAjdu <ivan.ajdu@gmail.com>
2024-03-08 18:43:38 +0100

    TMPro for Score + Script draft for increasing score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets; 
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
+    private int score;
+
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTargets());
+        score = 0;
+        UpdateScore(0);
     }
 
     // Update is called once per frame
@@ -26,6 +32,14 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate);
             int targetIndex = Random.Range(0, targets.Count);
             Instantiate(targets[targetIndex]);
+
+            UpdateScore(5);
         }
+    }
+
+    private void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score : " + score;
     }
 }
